2e8a2d2df2be436e610df2fdeb0f098b4056dd1b
Ralph-tech <58685004+Ralph-tech@users.noreply.github.com>
2021-06-07 13:48:49 +0300

    Add weather class for api handling

---
+++ src/modules/weather.js
@@ -0,0 +1,23 @@
+class Weather {
+  constructor(city){
+    this.api = '1bf7ab913a3e3c155665a70688f42c53';
+    this.city = city;
+  }
+
+  async getweather() {
+    const responseData = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city},uk&APPID=${this.api}`)
+
+    const response = await responseData.json();
+
+    return response;
+  }
+
+
+  changeLocation(city) {
+    this.city = city;
+  }
+}
+
+
+
+
